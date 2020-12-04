a7555a6bcde60ea3b16f36138bba8aed50d0b5b6
loulou123546 <louis.maillard@protonmail.com>
2020-12-04 01:25:40 +0100

    Update main.js

--- bots/discord.js/main.js
+++ bots/discord.js/main.js
@@ -1,5 +1,7 @@
+const process = require("process")
 const Discord = require("discord.js");
 const client = new Discord.Client();
+
 
 client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -12,3 +14,11 @@ client.on("message", msg => {
 });
 
 client.login(require("./token.js").secret_token);
+
+
+
+
+// Keep these line on end of file, it allow a better display in Manager and fix a bug about killing node process
+process.stdin.resume();process.stdin.setEncoding('utf8');
+process.stdin.on('data',(chunk)=>{if(chunk=="quit"||chunk=="quit\n"){process.exit()}});
+console.log("Bot ready"); // Keep this at end of file, it's used to mark server as Started
